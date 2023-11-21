1cd92dad8c2cbdd58feeca5f4fac05d54dbbbdfb
linl <837706731@qq.com>
2023-11-21 19:59:33 +0800

    111

---
+++ src/views/admin/operation/Operation.vue
@@ -0,0 +1,40 @@
+<template>
+ <div>
+
+ </div>
+</template>
+
+<script>
+ export default {
+   name: 'Operation',
+   props: {
+   },
+   components: {
+
+   },
+   data () {
+     return {
+
+     }
+   },
+   methods: {
+
+   },
+   mounted() {
+
+   },
+   watch: {
+
+   },
+   computed: {
+
+   },
+   filters: {
+
+   }
+ }
+</script>
+
+<style lang='less' scoped>
+
+</style>    
