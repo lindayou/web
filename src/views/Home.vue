0cc46c45d07da3a1d5dd8cdc3d176abd066dd207
linlong <837706731@qq.com>
2023-11-22 22:53:17 +0800

    qqq

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,11 +19,16 @@
 </el-card>
      </el-col>
    </el-row>
+   <el-row>
+
+    <div id="charts" style="width: 100% ; height: 300px;"></div>
+   </el-row>
   
  </div>
 </template>
 
 <script>
+import * as echarts from 'echarts';
  export default {
    name: 'home',
    props: {
@@ -40,6 +45,26 @@
 
    },
    mounted() {
+
+    var myChart = echarts.init(document.getElementById('charts'));
+// 绘制图表
+myChart.setOption({
+  title: {
+    text: 'ECharts 入门示例'
+  },
+  tooltip: {},
+  xAxis: {
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+  },
+  yAxis: {},
+  series: [
+    {
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20]
+    }
+  ]
+});
 
    },
    watch: {
